bb4f79b02bac02eaa9041eb64414f14ea2a7e8e5
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-20 13:40:28 -0800

    takes in valid operand1 and operator in order

--- script.js
+++ script.js
@@ -41,9 +41,7 @@ function operate(operand1, operator, operand2) {
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.srcElement.textContent);
     let userInput = e.srcElement.textContent;
-    console.log(isNaN(userInput));
     evaluteInput(userInput);
   });
 });
@@ -51,11 +49,45 @@ buttons.forEach((button) => {
 // create a function that checks the values of the operands and the operator to see if the input is valid and what variable it should be stored if it is
 
 function evaluteInput(buttonPress) {
-  if (operand1 === 0 && isNaN() == false) {
-    operand1 = +buttonPress;
-  }
-}
+  const operators = ["+", "-", "*", "/"];
 
+  // checks if a button press is one of the four operators
+  const checkForOperators = operators.some((operator) =>
+    buttonPress.includes(operator)
+  );
+
+  // checks for if a operator already exists when a operator is selected
+  if (checkForOperators && operator) {
+    console.log("operator exists already");
+    return;
+  }
+
+  // checks if a equal sign is pressed without a second operand
+  if (buttonPress === "=" && !operand2) {
+    console.log("cant use = without second operand");
+    return;
+  }
+
+  // check if buttonPressed is a num and reassign operand1 if it is a 0 otherwise add onto it
+  if (operand1 === 0 && !isNaN(buttonPress) && !operator) {
+    console.log("reassign operand1");
+    operand1 = buttonPress;
+  } else if (operand1 != 0 && !isNaN(buttonPress) && !operator) {
+    console.log("add onto operand 1");
+    operand1 += buttonPress;
+  }
+
+  // assigns an operator if one is selected and one does not already exist
+  if (checkForOperators && !operator) {
+    console.log("assign new operator");
+    operator = buttonPress;
+  }
+
+  // // assigns a num if operand1 and operator exist
+  // if (operand1 && operator && !operand2 && !isNaN(buttonPress)) {
+  //   operand2 = buttonPress;
+  // }
+}
 // store both number inputs and call the operate function when the user hits the = button
 
 // update the display with the result of the operation when the = sign is pressed
