f1327706632b076fced813dea16ab23b91bfdae8
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-19 21:56:36 -0800

    add initial file setup and pseudocode

---
+++ script.js
@@ -0,0 +1,31 @@
+// create a function that does addition
+function add(num1, num2) {
+  return num1 + num2;
+}
+// create a function that does subtraction
+
+// create a function that does multiplication
+
+// create a function that does division
+
+// create three variable two operands and a operator
+
+// create a function operate thate takes the two operands and a operator and calls one of the math functions
+
+// create functions that populate the display when you click the buttons on the calculator and store that value in a variable
+
+// store both number inputs and call the operate function when the user hits the = button
+
+// update the display with the result of the operation when the = sign is pressed
+
+// evaluate one pair of numbers at a time and update the first variable with the result along with then allowing only a next operator
+
+// round answers with long decimals to prevent overflow
+
+// don't allow the = button to work prematurally
+
+// make sure there is a clear button that wipes out all data
+
+// display a error message if the user tries to divide by 0
+
+// prevent a operator from being used twice in a row
