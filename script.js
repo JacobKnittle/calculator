8c177f67f64152c89adca70f8b662a57523843b4
JacobKnittle <jacobdknittle@gmail.com>
2026-09-05 09:12:12 -0700

    Prevent = button from working if not all variables have values

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ buttonsContainer.addEventListener(('click'), (e) => {
 
 function updateValuesAndDisplay(userInput) {
     if (screen.value.length > 16) return
+    if (userInput === '=' && (num1 === undefined || num2 === undefined || operator === 'undefined')) return
     if (num1 === 0 && operator === undefined && Number.isInteger(+userInput)) {
         num1 = +userInput
         screen.value = num1
